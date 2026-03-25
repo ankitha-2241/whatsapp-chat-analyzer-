f2db33f67c085b35681895d73654062405b4ad21
ankitha-2241 <154681115+ankitha-2241@users.noreply.github.com>
2026-03-25 16:20:43 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -4,11 +4,6 @@ import pandas as pd
 from collections import Counter
 import emoji
 
-from textblob import TextBlob
-import nltk
-nltk.download('punkt')
-
-
 extract = URLExtract()
 
 def fetch_stats(selected_user,df):
